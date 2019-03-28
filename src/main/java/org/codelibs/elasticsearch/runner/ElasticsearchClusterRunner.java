e30abb848a8d260f11c14112fdec4aabdaa3d107
Shinsuke Sugaya <shinsuke@apache.org>
2019-03-28 18:39:17 +0900

    elasticsearch 7.0.0-rc1

--- src/main/java/org/codelibs/elasticsearch/runner/ElasticsearchClusterRunner.java
+++ src/main/java/org/codelibs/elasticsearch/runner/ElasticsearchClusterRunner.java
@@ -120,7 +120,7 @@ public class ElasticsearchClusterRunner implements Closeable {
             "org.elasticsearch.search.aggregations.matrix.MatrixAggregationPlugin", //
             "org.elasticsearch.analysis.common.CommonAnalysisPlugin", //
             "org.elasticsearch.ingest.common.IngestCommonPlugin", //
-            "org.elasticsearch.ingest.geoip.IngestGeoIpPlugin", //
+            // "org.elasticsearch.ingest.geoip.IngestGeoIpPlugin", //
             "org.elasticsearch.ingest.useragent.IngestUserAgentPlugin", //
             "org.elasticsearch.script.expression.ExpressionPlugin", //
             "org.elasticsearch.script.mustache.MustachePlugin", //
