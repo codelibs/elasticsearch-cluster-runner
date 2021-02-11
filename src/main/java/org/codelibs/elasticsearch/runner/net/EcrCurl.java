44b65008d59e74661e6e64b1496752b843240df4
Shinsuke Sugaya <shinsuke@apache.org>
2021-02-11 11:37:55 +0900

    elasticsearch 7.11.0

--- src/main/java/org/codelibs/elasticsearch/runner/net/EcrCurl.java
+++ src/main/java/org/codelibs/elasticsearch/runner/net/EcrCurl.java
@@ -1,17 +1,18 @@
 /*
- * Copyright 2012-2020 CodeLibs Project and the Others.
+ * Copyright 2012-2021 CodeLibs Project and the Others.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package org.codelibs.elasticsearch.runner.net;
 
